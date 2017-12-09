97bf2229e6b5c63b6399086b72b532333753aa5b
JohnM000 <johnmckenna425@gmail.com>
2017-12-08 16:06:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 function playCheer(e){
 	document.getElementById("crowd-noise").play();
 }
+function playMullet(e){
+	document.getElementById("crowd-noise").play();
+}
+function playDang(e){
+	document.getElementById("crowd-noise").play();
+}
