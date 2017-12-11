b4d7cdd0301ed0446ffbc091a5a68ea3239b235c
JohnM000 <johnmckenna425@gmail.com>
2017-12-11 12:48:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,3 @@ function playFlabbit(e){
 function playNibblets(e){
 	document.getElementById("sweet-nibblets").play();
 }
- function changeImage(e) {
-
-        if (document.getElementById("imgClickAndChange").src = "brc.jpg") 
-        {
-            document.getElementById("imgClickAndChange").src = "mosaic1.jpg";
-        }
-        else 
-        {
-            document.getElementById("imgClickAndChange").src = "mosaic1.jpg";
-        }
-    }
